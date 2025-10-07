6dc30f10f8ba4a478768738b19049512dc99004b
Sébastien Nouira Huet <sebastien.nouira--huet@ensea.fr>
2025-10-07 11:16:10 +0200

    [ADD + FIX] Added free_3is method and fixed multiple conception bugs

--- main.c
+++ main.c
@@ -5,6 +5,8 @@
 #define MAP_FAILED (void*)SYS_CALL_ERROR
 #define EXIT_SUCCESS (0)
 #define EXIT_FAILURE (-1)
+#define TRUE 1
+#define FALSE 0
 
 #define MAGIC_NUMBER 0x0123456789ABCDEFL
 
@@ -21,26 +23,66 @@ typedef struct HEADER_TAG {
 
 
 void* malloc_3is(size_t size);
-void free_3is(const void* ptr);
+void free_3is(void* ptr);
 long long check_magic_number(void* ptr);
 
-HEADER* get_head_of_size(size_t size);
+HEADER* get_block_of_size(size_t size);
+HEADER* extract_block(HEADER* block);
+
+void test_allocation();
+void test_free();
 
 HEADER* head = NULL;
+int do_print_allocation_type = 0;
 
 
-HEADER* get_head_of_size(size_t size) {
+void enable_allocation_type_printing() {
+    do_print_allocation_type = TRUE;
+}
+
+void disable_allocation_type_printing() {
+    do_print_allocation_type = FALSE;
+}
+
+
+HEADER* get_block_of_size(size_t size) {
     HEADER* cur_head = head;
-    while (cur_head != NULL) {
-        if (cur_head->bloc_size <= size) {
-            // Might need to split the block for optimisation
-            cur_head->ptr_next->ptr_prev = cur_head->ptr_prev;
-            cur_head->ptr_prev->ptr_next = cur_head->ptr_next;
-            return cur_head;
+    while (cur_head) {
+        if (cur_head->bloc_size >= size) {
+            return extract_block(cur_head);
         }
-        cur_head = cur_head->ptr_next;
+        cur_head = cur_head->ptr_prev;
     }
     return NULL;
+}
+
+HEADER* extract_block(HEADER* block) {
+    if (block == NULL) {
+        perror("extract_block: NULL block\n");
+        return NULL;
+    }
+
+    // Might need to split the block for optimisation
+    if (block == head) {
+        if (block->ptr_prev) {
+            head = block->ptr_prev;
+        } else {
+            head = NULL;
+        }
+    }
+
+    if (block->ptr_next) {
+        block->ptr_next->ptr_prev = block->ptr_prev;
+    }
+
+    if (block->ptr_prev) {
+        block->ptr_prev->ptr_next = block->ptr_next;
+    }
+
+    block->ptr_next = NULL;
+    block->ptr_prev = NULL;
+
+    return block;
 }
 
 void* malloc_3is(const size_t size) {
@@ -48,15 +90,18 @@ void* malloc_3is(const size_t size) {
         return NULL;
     }
 
-    HEADER* available_block = get_head_of_size(size);
-    if (available_block != NULL) {
-        return available_block;
+    HEADER* available_block = get_block_of_size(size);
+    if (available_block) {
+        if (do_print_allocation_type) {
+            printf("malloc_3is: Reusing allocated memory\n");
+        }
+        return ++available_block;
     }
 
     void* start_ptr = sbrk((long) (size + HEADER_SIZE + MAGIC_NUMBER_SIZE));
     void* end_ptr = sbrk(0);
     if (end_ptr == MAP_FAILED) {
-        perror("malloc_3is: sbrk failed");
+        perror("malloc_3is: sbrk failed\n");
         return NULL;
     }
 
@@ -70,7 +115,31 @@ void* malloc_3is(const size_t size) {
     start_ptr += HEADER_SIZE + size;
     *((long long*) start_ptr) = MAGIC_NUMBER;
 
+    if (do_print_allocation_type) {
+        printf("malloc_3is: New allocated memory\n");
+    }
     return header_ptr;
+}
+
+void free_3is(void* ptr) {
+    if (ptr == NULL) {
+        return;
+    }
+
+    if (check_magic_number(ptr)) {
+        perror("free_3is: bad magic number\n");
+        return;
+    }
+
+    HEADER* head_ptr = (HEADER*) ptr;
+    head_ptr--;
+    if (head == NULL) {
+        head = head_ptr;
+    } else {
+        head->ptr_next = head_ptr;
+        head->ptr_prev = head;
+        head = head_ptr;
+    }
 }
 
 long long check_magic_number(void* ptr) {
@@ -78,29 +147,58 @@ long long check_magic_number(void* ptr) {
         return 0;
     }
 
-    const HEADER* head_ptr = (HEADER*) ptr;
+    // Expects a pointer after the header block
+    HEADER* head_ptr = (HEADER*) ptr;
+    head_ptr--;
     long long expected_magic_number = head_ptr->magic_number;
 
-    void* tmp_ptr = ptr;
-    tmp_ptr += HEADER_SIZE + head_ptr->bloc_size;
-    long long actual_magic_number = *((long long*) tmp_ptr);
+    ptr += head_ptr->bloc_size;
+    long long actual_magic_number = *((long long*) ptr);
 
     return expected_magic_number - actual_magic_number;
 }
 
 
+void test_allocation() {
+    printf("\n===================== TEST ALLOC =====================\n");
+    disable_allocation_type_printing();
+
+    void* test = malloc_3is(0x004);
+    printf("\t- test_allocation: 1st alloc %p, magic number check: %lld\n", test, check_magic_number(test));
+
+    test = malloc_3is(0x0f0);
+    printf("\t- test_allocation: 2nd alloc %p, magic number check: %lld\n", test, check_magic_number(test));
+
+    // We accept the memory leak here, so that we don't interfere with the test_free() method
+}
+
+void test_free() {
+    printf("\n==================== TEST FREE =====================\n");
+    enable_allocation_type_printing();
+
+    void* test = malloc_3is(0x0f0);
+    printf("\t- test_free: 1st allocation %p\n", test);
+
+    free_3is(test);
+    test = malloc_3is(0x0f0);
+    printf("\t- test_free: 2nd allocation %p\n", test);
+
+    free_3is(test);
+    test = malloc_3is(0xfff);
+    printf("\t- test_free: 3rd allocation %p\n", test);
+
+    free_3is(test);
+    test = malloc_3is(0x0f0);
+    printf("\t- test_free: 4th allocation %p\n", test);
+}
+
+
 int main(void) {
     printf("TP2: allocateur mémoire\n");
-    void* test;
 
-    test = malloc_3is(4);
-    printf("test 1: %p, magic number check: %lld\n", test, check_magic_number(test));
-
-    test = malloc_3is(0xf4);
-    printf("test 2: %p, magic number check: %lld\n", test, check_magic_number(test));
-
-    test = malloc_3is(4);
-    printf("test 3: %p, magic number check: %lld\n", test, check_magic_number(test));
+    test_allocation();
+    test_free();
+    test_allocation();
 
     return EXIT_SUCCESS;
 }
