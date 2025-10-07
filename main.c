c7be8b32740566b8725f7556f191b78fbe9a7d60
Sébastien Nouira Huet <sebastien.nouira--huet@ensea.fr>
2025-10-07 09:14:12 +0200

    [ADD] malloc_3is checks for available blocks first

--- main.c
+++ main.c
@@ -1,43 +1,50 @@
 #include <stdio.h>
-#include<unistd.h>
+#include <unistd.h>
 
 #define SYS_CALL_ERROR (-1)
 #define SBRK_ERROR (void*)SYS_CALL_ERROR
-
-#define MINIMUM_BLOCK_SIZE 0xff
+#define EXIT_SUCCESS (0)
+#define EXIT_FAILURE (-1)
 
 
 typedef struct HEADER_TAG {
     struct HEADER_TAG* ptr_next;
+    struct HEADER_TAG* ptr_prev;
     size_t bloc_size;
     long magic_number;
 } HEADER;
 
 #define HEADER_SIZE sizeof(HEADER)
-#define BLOCK_SIZE (MINIMUM_BLOCK_SIZE + HEADER_SIZE)
 
 void* malloc_3is(size_t size);
 // void free_3is(const void* ptr);
 
-size_t get_size_to_increase(size_t size_requested);
+HEADER* get_head_of_size(size_t size);
 
 HEADER* head = NULL;
 
 
-// size_t get_size_to_increase(size_t size_requested) {
-//     size_t minimum_to_allocate = size_requested + HEADER_SIZE;
-//     size_t already_allocated = 0;
-//     if (head != NULL) {
-//
-//     }
-//
-//     const size_t nb_blocks = (minimum_to_allocate - already_allocated) / MINIMUM_BLOCK_SIZE;
-//     return (nb_blocks + 1) * MINIMUM_BLOCK_SIZE;
-// }
+HEADER* get_head_of_size(size_t size) {
+    HEADER* cur_head = head;
+    while (cur_head != NULL) {
+        if (cur_head->bloc_size <= size) {
+            cur_head->ptr_next->ptr_prev = cur_head->ptr_prev;
+            cur_head->ptr_prev->ptr_next = cur_head->ptr_next;
+            return cur_head;
+        }
+        cur_head = cur_head->ptr_next;
+    }
+    return NULL;
+}
 
 void* malloc_3is(const size_t size) {
     if (size == 0) {
         return NULL;
+    }
+
+    HEADER* available_block = get_head_of_size(size);
+    if (available_block != NULL) {
+        return available_block;
     }
 
     void* start_ptr = sbrk(size);
@@ -46,12 +53,18 @@ void* malloc_3is(const size_t size) {
         perror("malloc_3is: sbrk failed");
         return NULL;
     }
-
     return start_ptr;
 }
 
 
 int main(void) {
     printf("TP2: allocateur mémoire\n");
-    return 0;
+
+    void* test = malloc_3is(4);
+    printf("test 1: %p\n", test);
+
+    test = malloc_3is(4);
+    printf("test 2: %p\n", test);
+
+    return EXIT_SUCCESS;
 }
